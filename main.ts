48882dd67a75e6fa0a136d96657ebe021e3bcf25
stemventuri <57156157+stemventuri@users.noreply.github.com>
2020-10-26 23:06:31 +0000

    Simplified

--- main.ts
+++ main.ts
@@ -1,12 +1,8 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 0) {
         ghost_detected = 1
-        ghost_detected_id = radio.receivedPacket(RadioPacketProperty.SerialNumber)
     }
 })
-function main_game () {
-	
-}
 function main_sound () {
     if (ghost_detected) {
         music.startMelody(music.builtInMelody(Melodies.Chase), MelodyOptions.Once)
@@ -32,14 +28,8 @@ function setup_torch () {
         basic.pause(1000)
     }
 }
-input.onButtonPressed(Button.AB, function () {
-    setup_new_game()
-})
 function torch_lowbeam () {
     pins.analogWritePin(AnalogPin.P2, 200)
-}
-function setup_new_game () {
-    ghost_detected_list = []
 }
 function torch_fullbeam () {
     pins.analogWritePin(AnalogPin.P2, 1023)
@@ -48,17 +38,13 @@ function setup_sound () {
     music.setTempo(120)
     music.setVolume(60)
 }
-let ghost_detected_list: number[] = []
-let ghost_detected_id = 0
 let ghost_detected = 0
 setup_radio()
 setup_torch()
 setup_sound()
-setup_new_game()
 basic.forever(function () {
     main_torch()
     main_radio()
     main_sound()
-    main_game()
     ghost_detected = 0
 })
